2cc22abb08e79935b8483720c1f834a79f445e7d
Sergey Andreev <asergey91@gmail.com>
2016-03-21 10:44:08 +0000

    I like trains

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,6 @@ class SearchesController < ApplicationController
   def search
     @search_string=params[:search_string]
     @search_string.downcase
-    @results=Product.where("name like ?", "%#{@search_string}%")
+    @results=Product.where("name ilike ?", "%#{@search_string}%")
   end
 end
